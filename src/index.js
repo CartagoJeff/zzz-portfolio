a8541830340d3e9f9b995ce39134bc0d5b3ff385
Jeff Cartago <80418764+CartagoJeff@users.noreply.github.com>
2025-02-01 16:59:28 +0800

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOMClient from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = ReactDOMClient.createRoot(document.getElementById('root'));
-root.render(<App />);
-
